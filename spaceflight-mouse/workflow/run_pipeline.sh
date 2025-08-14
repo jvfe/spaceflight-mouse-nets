6fa4c77052ae26484c048b760616474a35f4a532
jvfe <jvfecav@gmail.com>
2025-08-14 14:51:41 -0300

    Update refs in script

--- spaceflight-mouse/workflow/run_pipeline.sh
+++ spaceflight-mouse/workflow/run_pipeline.sh
@@ -4,8 +4,8 @@ set -e
 # Run the pipeline with adjusted paths
 nextflow run jvfe/simplernaseq \
     --input data/samplesheet.csv \
-    --fasta data/references/chr22.fa \
-    --gtf data/references/chr22.gtf \
+    --fasta data/references/Mus_musculus.GRCm39.dna.chromosome.19.fa \
+    --gtf data/references/Mus_musculus.GRCm39.112.chr19.gtf \
     --outdir results \
     -profile docker,small_genome \
     -resume
